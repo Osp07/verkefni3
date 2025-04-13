8c19bed1b58148dee57d472fb3d1c67eef860dfa
Osp07 <ingibjorg.osp.finns@gmail.com>
2025-04-13 22:40:51 +0000

    Add files via upload

---
+++ Vatn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vatn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // ef að eitthvað kemur við varnið sem tengt er við trigger
+        if (other.tag == "Player")
+        {
+            // ef að leikmaður kemur við vatnið
+            Debug.Log("Drukknaði");
+            // koma upp skilaboð sem segja að leikmaður hafi drukknað
+            SceneManager.LoadScene(0);
+            // við förum yfir á byrjunarsenu
+            Ovinur.health = 70;
+            // líf endurstillast á 70
+            Kassi.count = 0;
+            // leikmaður byrjar aftur með 0 stig
+
+        }
+    }
+}
